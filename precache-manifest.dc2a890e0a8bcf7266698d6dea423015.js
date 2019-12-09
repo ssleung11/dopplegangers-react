20e333c12f1e0d2d0b63ac35d2c4cf1627dd0e44
Shirley Leung <shirley@shirleys-mbp.home>
2019-12-08 22:23:56 -0500

    Updates

---
+++ precache-manifest.dc2a890e0a8bcf7266698d6dea423015.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e5e1bf68abca5577dca26a9c59aa15",
+    "url": "/dopplegangers-react/index.html"
+  },
+  {
+    "revision": "570838fae4b8e9d4229c",
+    "url": "/dopplegangers-react/static/css/main.d492e5ec.chunk.css"
+  },
+  {
+    "revision": "0969b988290e307e9009",
+    "url": "/dopplegangers-react/static/js/2.5ded3b62.chunk.js"
+  },
+  {
+    "revision": "570838fae4b8e9d4229c",
+    "url": "/dopplegangers-react/static/js/main.078a078c.chunk.js"
+  },
+  {
+    "revision": "b47b5de7349a3abad6da",
+    "url": "/dopplegangers-react/static/js/runtime-main.5449ee8d.js"
+  }
+]);
